c45970c39a35cadc6ecfec1de2e1905b6238c15c
Phuoc Huynh <huynhphvan@gmail.com>
2021-03-15 17:30:51 +0700

    update ui

--- src/screens/HomePageScreen/index.tsx
+++ src/screens/HomePageScreen/index.tsx
@@ -45,7 +45,7 @@ const Button = styled.button`
 `;
 
 const Section = styled.div`
-    padding: 100px 0;
+    padding: 50px 0;
 `;
 
 const defaultTicker = {
@@ -63,7 +63,7 @@ const BASE_MARKET_URL = "https://www.lukutex.com/api/v2/peatio/public/markets";
 export const HomePageScreen = () => {
 
     const market_ids = [
-        'btcusd', 'ethbtc', 'btcusd', 'ethbtc', 'btcusd', 'ethbtc', 'btcusd'
+        'btc/usdt', 'eth/usdt', 'eth/usdt', 'eth/usdt', 'eth/usdt', 'eth/usdt', 'eth/usdt'
     ]
 
     const [KlineState1, setKline1State] = React.useState<{ pv: string }>();
@@ -85,7 +85,7 @@ export const HomePageScreen = () => {
 
     const fetchMarketsKlines = async (marketId: string, from: number, to: number) => {
         try {
-            const klines = await axios.get(`${BASE_MARKET_URL}/${marketId}/k-line?period=30&time_from=${from}&time_to=${to}`);
+            const klines = await axios.get(`${BASE_MARKET_URL}/${marketId.split('/').join('')}/k-line?period=30&time_from=${from}&time_to=${to}`);
             return klines.data.map((kline, index) => { return { pv: kline[3] } });
         } catch (error) {
             return [];
@@ -126,8 +126,8 @@ export const HomePageScreen = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <Title>CircleEx Exchange</Title>
-                        <SubTitle>Buy and sell cryptocurrencies commission-free on the world’s most secure investment platform.</SubTitle>
+                        <Title>CircleEX Exchange</Title>
+                        <SubTitle>First Governance Community Exchange For the community By the Community</SubTitle>
                     </div>
                 </div>
                 <div className="row">
@@ -177,21 +177,26 @@ export const HomePageScreen = () => {
 
     const renderMarket = () => {
         const MarketChart = (data: any, marketID: string) => {
-            const market = markets.find(market => market.id.toLowerCase() === marketID.toLowerCase());
+            const market = markets.find(market => market.base_unit.toLowerCase() === marketID.split('/')[0].toLowerCase());
+
             if (market) {
                 const baseCurrency = market.name.split('/')[0];
-                const quoteCurrency = market.name.split('/')[1];
                 const last = Number((marketTickers[market.id] || defaultTicker).last);
                 const open = Number((marketTickers[market.id] || defaultTicker).open);
                 const price_change_percent = (marketTickers[market.id] || defaultTicker).price_change_percent;
                 const change = (+last - +open);
                 // color
-                const marketChangeColor = +(change || 0) < 0 ? "#e63946" : "#2ec4b6";
+                const marketChangeColor = +(change || 0) < 0 ? "#91121D" : "#88A190ff";
                 return (
+
                     <div className="container">
                         <div className="row">
-                            <div className="col-12">
-                                <img width="30px" height="30px" src={findIcon(baseCurrency)} alt="" /> <span style={{ fontSize: '1.2rem' }} className="text-white">{baseCurrency.toUpperCase()}</span> / <span style={{ fontSize: '1rem' }} className="text-secondary">{quoteCurrency.toUpperCase()}</span>
+                            <div className="col-12 d-flex justify-content-between">
+                                <div>
+                                    <img width="30px" height="30px" src={findIcon(baseCurrency)} alt="" />
+                                    <span style={{ fontSize: '1.2rem', fontWeight: 'bold', margin: '5px' }} className="text-white">{baseCurrency.toUpperCase()}</span>
+                                </div>
+                                <span style={{ color: '#fff', padding: '0.5rem 1rem', backgroundColor: '#414455ff', borderRadius: '1rem', fontWeight: 'bold' }}>24H</span>
                             </div>
                         </div>
                         <div className="row mt-3">
@@ -202,120 +207,129 @@ export const HomePageScreen = () => {
                                 <span style={{ marginRight: '5px', color: marketChangeColor, fontWeight: 'bold' }}>{price_change_percent}</span>
                             </div>
                         </div>
-                        <div className="row">
+                        <div className="row mt-5">
                             <div className="col-12" >
                                 <ResponsiveContainer width='100%' aspect={4.0 / 1.0}>
                                     <AreaChart data={data}>
-                                        <Area type="monotone" dataKey="pv" stroke="#8884d8" fill="#8884d8" strokeWidth={2} />
+                                        <Area type="monotone" dataKey="pv" stroke="#fff" fill="transparent" strokeWidth={2} />
                                     </AreaChart>
                                 </ResponsiveContainer>
                             </div>
                         </div>
                     </div>
+
                 );
             }
             return '';
         }
 
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-3">
-                        {MarketChart(KlineState1, market_ids[0])}
-                    </div>
+            <Section>
+                <div className="container" style={{backgroundColor: '#313445ff', padding: '3rem 1.5rem', borderRadius: '1rem'}}>
+                    <div className="row">
+                        <div className="col-3">
+                            {MarketChart(KlineState1, market_ids[0])}
+                        </div>
 
-                    <div className="col-3">
-                        {MarketChart(KlineState2, market_ids[1])}
-                    </div>
-                    <div className="col-3">
-                        {MarketChart(KlineState3, market_ids[2])}
-                    </div>
+                        <div className="col-3">
+                            {MarketChart(KlineState2, market_ids[1])}
+                        </div>
+                        <div className="col-3">
+                            {MarketChart(KlineState3, market_ids[2])}
+                        </div>
 
-                    <div className="col-3">
-                        {MarketChart(KlineState4, market_ids[3])}
+                        <div className="col-3">
+                            {MarketChart(KlineState4, market_ids[3])}
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-3">
+                            {MarketChart(KlineState5, market_ids[4])}
+                        </div>
+
+                        <div className="col-3">
+                            {MarketChart(KlineState6, market_ids[5])}
+                        </div>
+                        <div className="col-3">
+                            {MarketChart(KlineState7, market_ids[6])}
+                        </div>
+                        <div className="col-3 d-flex justify-content-center align-items-center">
+                            <button className="btn" style={{ color: '#3D8189ff', backgroundColor: '#2E4152ff', padding: '0.5rem 1rem' }}>View All</button>
+                        </div>
                     </div>
                 </div>
-                <div className="row mt-5">
-                    <div className="col-3">
-                        {MarketChart(KlineState5, market_ids[4])}
-                    </div>
+            </Section>
 
-                    <div className="col-3">
-                        {MarketChart(KlineState6, market_ids[5])}
-                    </div>
-                    <div className="col-3">
-                        {MarketChart(KlineState7, market_ids[6])}
-                    </div>
-                    <div className="col-3 d-flex justify-content-center align-items-center">
-                        <button className="btn" style={{ color: '#fff', backgroundColor: '#259DA8', padding: '0.5rem 1rem' }}>View All</button>
-                    </div>
-                </div>
-            </div>
         );
     }
 
     const renderSupport = () => (
-        <div className="container text-white" style={{ padding: '100px 0' }}>
-            <div className="row">
-                <div className="col-3 text-center">
-                    <img className="img-fluid w-50" src={SupportPNG} alt="support" />
-                    <h3 className="mt-3">24/7 Support</h3>
-                    <span>Got a problem? Just get in touch. Our support team is available 24/7.</span>
-                </div>
-                <div className="col-3 text-center">
-                    <img className="img-fluid w-50" src={BlogPNG} alt="support" />
-                    <h3 className="mt-3">Circle Blog</h3>
-                    <span>News and updates from the world’s leading cryptocurrency exchange.</span>
-                </div>
-                <div className="col-3 text-center">
-                    <img className="img-fluid w-50" src={CommunityPNG} alt="support" />
-                    <h3 className="mt-3">Community</h3>
-                    <span>Circle Exchange is global. Join the discussion in our worldwide communities.</span>
-                </div>
-                <div className="col-3 text-center">
-                    <img className="img-fluid w-50" src={CareersPNG} alt="support" />
-                    <h3 className="mt-3">Careers</h3>
-                    <span>Help build the future of technology. Start your new career at Circle Exchange.</span>
+        <Section>
+            <div className="container text-white">
+                <div className="row">
+                    <div className="col-3 text-center">
+                        <img className="img-fluid w-50" src={SupportPNG} alt="support" />
+                        <h3 className="mt-3">24/7 Support</h3>
+                        <span>Got a problem? Just get in touch. Our support team is available 24/7.</span>
+                    </div>
+                    <div className="col-3 text-center">
+                        <img className="img-fluid w-50" src={BlogPNG} alt="support" />
+                        <h3 className="mt-3">Circle Blog</h3>
+                        <span>News and updates from the world’s leading cryptocurrency exchange.</span>
+                    </div>
+                    <div className="col-3 text-center">
+                        <img className="img-fluid w-50" src={CommunityPNG} alt="support" />
+                        <h3 className="mt-3">Community</h3>
+                        <span>Circle Exchange is global. Join the discussion in our worldwide communities.</span>
+                    </div>
+                    <div className="col-3 text-center">
+                        <img className="img-fluid w-50" src={CareersPNG} alt="support" />
+                        <h3 className="mt-3">Careers</h3>
+                        <span>Help build the future of technology. Start your new career at Circle Exchange.</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Section>
+
     );
 
     const renderPoster = () => {
         return (
-            <div style={{ padding: '150px 0', backgroundColor: '#292D3F' }}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <div className="row">
-                                <div className="col-12">
-                                    <h1>Trade anytime and anywhere</h1>
-                                    <h4 className="mt-5">
-                                        Download CircleEx APP, you will be able to easily at any time, anywhere trading global mainstream, popular digital assets.
-                                    </h4>
+            <Section>
+                <div style={{ padding: '150px 0', backgroundColor: '#292D3F' }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <h1>Trade anytime and anywhere</h1>
+                                        <h4 className="mt-5">
+                                            Download CircleEx APP, you will be able to easily at any time, anywhere trading global mainstream, popular digital assets.
+                                        </h4>
+                                    </div>
+                                </div>
+                                <div className="row mt-5">
+                                    <div className="col-6">
+                                        <img className="img-fluid" src={AppStore} alt="apple+store" />
+                                    </div>
+                                    <div className="col-6">
+                                        <img className="img-fluid" src={GooglePlay} alt="google+play" />
+                                    </div>
+                                    <div className="col-6 mt-3">
+                                        <img className="img-fluid" src={AndroidAPK} alt="android+apk" />
+                                    </div>
+                                    <div className="col-6 mt-3">
+                                        <img width="255px" src={Scan} alt="scan+qrcode" />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="row mt-5">
-                                <div className="col-6">
-                                    <img className="img-fluid" src={AppStore} alt="apple+store" />
-                                </div>
-                                <div className="col-6">
-                                    <img className="img-fluid" src={GooglePlay} alt="google+play" />
-                                </div>
-                                <div className="col-6 mt-3">
-                                    <img className="img-fluid" src={AndroidAPK} alt="android+apk" />
-                                </div>
-                                <div className="col-6 mt-3">
-                                    <img width="255px" src={Scan} alt="scan+qrcode" />
-                                </div>
+                            <div className="col-6">
+                                <img className="img-fluid" src={Exchange} alt="exchange" />
                             </div>
-                        </div>
-                        <div className="col-6">
-                            <img className="img-fluid" src={Exchange} alt="exchange" />
                         </div>
                     </div>
                 </div>
-            </div>
+            </Section>
         );
     }
 
