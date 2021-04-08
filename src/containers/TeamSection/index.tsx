f1362b4ae2a75715e633fcd29e6dda44dde2a2f8
vinhtran91 <68156540+vinhtran91@users.noreply.github.com>
2021-04-08 14:12:41 +0700

    update member section

--- src/containers/TeamSection/index.tsx
+++ src/containers/TeamSection/index.tsx
@@ -28,182 +28,199 @@ export const TeamSection = () => {
     const [teamImageIndexState, setTeamImageIndexState] = React.useState(0);
     const [disabledButtonState, setDisabledButtonState] = React.useState(false);
 
-        const teams = [
-            {
-                large: 'https://fakeimg.pl/400x600/?text=1&font=GoogleSans',
-                medium: 'https://fakeimg.pl/200x300/?text=1&font=GoogleSans',
-                name: 'Huynh Van Phuoc',
-                roll: 'Reactjs Developer',
-                linkedin: 'https://www.linkedin.com',
-                description: 'Lorem asd213'
-            },
-            {
-                large: 'https://fakeimg.pl/400x600/?text=2&font=GoogleSans',
-                medium: 'https://fakeimg.pl/200x300/?text=2&font=GoogleSans',
-                name: 'Le Thanh Dat',
-                roll: 'Reactjs Developer',
-                linkedin: 'https://www.linkedin.com',
-                description: 'Lorem asd213'
-            },
-            {
-                large: 'https://fakeimg.pl/400x600/?text=3&font=GoogleSans',
-                medium: 'https://fakeimg.pl/200x300/?text=3&font=GoogleSans',
-                name: 'Ngo Nhat Duy',
-                roll: 'Reactjs Developer',
-                linkedin: 'https://www.linkedin.com',
-                description: 'Lorem asd213'
-            },
-            {
-                large: 'https://fakeimg.pl/400x600/?text=4&font=GoogleSans',
-                medium: 'https://fakeimg.pl/200x300/?text=4&font=GoogleSans',
-                name: 'Huynh Van Phuoc',
-                roll: 'Reactjs Developer',
-                linkedin: 'https://www.linkedin.com',
-                description: 'Lorem asd213'
-            },
+    const teams = [
+        {
+            large: 'https://i.imgur.com/ibxsOOc.png',
+            medium: 'https://i.imgur.com/NKJh3rA.png',
+            name: 'Tran Van Vinh',
+            roll: 'CEO of ZoziTech and CX Exchange',
+            linkedin: 'https://www.linkedin.com/in/vinh-tran-88237620a/',
+            description: ''
+        },
+        {
+            large: 'https://i.imgur.com/719PzQu.png',
+            medium: 'https://i.imgur.com/avcxjzo.png',
+            name: 'Huynh Van Phuoc',
+            roll: 'Blockchain and NodeJS Developer',
+            linkedin: 'https://www.linkedin.com/in/phuochuynh892/',
+            description: ''
+        },
+        {
+            large: 'https://i.imgur.com/TRJ4uWc.jpg',
+            medium: 'https://i.imgur.com/U33Yui3.jpg',
+            name: 'Le Thanh Dat',
+            roll: 'Blockchain and NodeJS Developer',
+            linkedin: 'https://www.linkedin.com/in/le-thanh-dat-9050451b4/',
+            description: ''
+        },
+        {
+            large: 'https://i.imgur.com/RcbaQlg.png',
+            medium: 'https://i.imgur.com/A3Jwx10.png',
+            name: 'Ngo Nhat Duy',
+            roll: 'Reactjs Developer',
+            linkedin: 'https://www.linkedin.com/in/nhat-duy-16803720a/',
+            description: ''
+        },
+        {
+            large: 'https://i.imgur.com/7KCaBrl.png',
+            medium: 'https://i.imgur.com/wmXpr0l.png',
+            name: 'Truong Thanh Huy',
+            roll: 'Reactjs Developer',
+            linkedin: 'https://www.linkedin.com/in/th%C3%A0nh-huy-tr%C6%B0%C6%A1ng-39a965209/',
+            description: ''
+        },
+        {
+            large: 'https://i.imgur.com/aLnnIAY.png',
+            medium: 'https://i.imgur.com/JN5pEwr.png',
+            name: 'Nguyen Thi Ha Duyen',
+            roll: 'Desinger',
+            linkedin: 'https://www.linkedin.com/in/duyen-nguyen-b9803420a/',
+            description: ''
+        },
 
-        ];
-        var settings = {
-            arrows: false,
-            dots: false,
-            speed: 500,
-            centerMode: true,
-            centerPadding: '10px',
-            initialSlide: 0,
-            infinity: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-        };
-
-        
-
-        const next = () => {
-            setDisabledButtonState(true);
-            setTimeout(() => {
-                setDisabledButtonState(false);
-            }, 1000);
-            slider.current.slickNext();
-            if(teamImageIndexState == teams.length - 1) setTeamImageIndexState(0);
-            else setTeamImageIndexState(teamImageIndexState + 1);
-        };
-
-        const previous = () => {
-            setDisabledButtonState(true);
-            setTimeout(() => {
-                setDisabledButtonState(false);
-            }, 1000);
-            slider.current.slickPrev();
-            if(teamImageIndexState == 0) setTeamImageIndexState(teams.length - 1);
-            else  setTeamImageIndexState(teamImageIndexState - 1);
-        };
-
-        React.useEffect(() => {
-            const interval = setInterval(() => {
-                next();
-            }, 2000);
-
-            return () => {
-                clearInterval(interval);
-            }
-        });
+    ];
+    var settings = {
+        arrows: false,
+        dots: false,
+        speed: 500,
+        centerMode: true,
+        centerPadding: '10px',
+        initialSlide: 0,
+        infinity: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    };
 
 
-        return (
-            <div className="container" style={{ padding: '50px 0' }}>
-                <div className="row">
-                    <div className="col-4">
-                        <img style={{ borderRadius: '5px', width: '100%' }} src={teams[teamImageIndexState].large} alt="" />
-                        <div className="mt-3 d-flex flex-row justify-content-between">
-                            <button
-                                style={{
-                                    width: '60px',
-                                    height: '60px',
-                                    borderRadius: '6px',
-                                    fontSize: '2rem',
-                                    backgroundColor: '#5DD1E1ff',
-                                    border: 'none',
-                                    outline: 'none',
-                                    color: '#fff'
-                                }}
-                                disabled={disabledButtonState}
-                                onClick={previous}
-                            >
-                                {'<'}
-                            </button>
-                            <div style={{ position: 'relative', width: '150px' }}>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
-                                    width: '100%', border: '1px solid #686A75ff'
-                                }}></div>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: '50%', left: '0', transform: 'translateY(-50%)',
-                                    width: `${(teamImageIndexState + 1) / teams.length * 100}%`, border: '1px solid #7BA6B0ff'
-                                }}></div>
-                                <div style={{
-                                    position: 'absolute',
-                                    top: '25%', left: '-5%', transform: 'translateY(-50%)',
-                                    width: '110%', display: 'flex', flexDirection: 'row',
-                                    justifyContent: 'space-between'
-                                }}>
-                                    <span>01</span>
-                                    <span>{teams.length}</span>
-                                </div>
 
+    const next = () => {
+        setDisabledButtonState(true);
+        setTimeout(() => {
+            setDisabledButtonState(false);
+        }, 1000);
+        slider.current.slickNext();
+        if (teamImageIndexState == teams.length - 1) setTeamImageIndexState(0);
+        else setTeamImageIndexState(teamImageIndexState + 1);
+    };
+
+    const previous = () => {
+        setDisabledButtonState(true);
+        setTimeout(() => {
+            setDisabledButtonState(false);
+        }, 1000);
+        slider.current.slickPrev();
+        if (teamImageIndexState == 0) setTeamImageIndexState(teams.length - 1);
+        else setTeamImageIndexState(teamImageIndexState - 1);
+    };
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            next();
+        }, 2000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    });
+
+
+    return (
+        <div className="container" style={{ padding: '50px 0' }}>
+            <div className="row">
+                <div className="col-4">
+                    <img style={{ borderRadius: '5px', width: '100%' }} src={teams[teamImageIndexState].large} alt="" />
+                    <div className="mt-3 d-flex flex-row justify-content-between">
+                        <button
+                            style={{
+                                width: '60px',
+                                height: '60px',
+                                borderRadius: '6px',
+                                fontSize: '2rem',
+                                backgroundColor: '#5DD1E1ff',
+                                border: 'none',
+                                outline: 'none',
+                                color: '#fff'
+                            }}
+                            disabled={disabledButtonState}
+                            onClick={previous}
+                        >
+                            {'<'}
+                        </button>
+                        <div style={{ position: 'relative', width: '150px' }}>
+                            <div style={{
+                                position: 'absolute',
+                                top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+                                width: '100%', border: '1px solid #686A75ff'
+                            }}></div>
+                            <div style={{
+                                position: 'absolute',
+                                top: '50%', left: '0', transform: 'translateY(-50%)',
+                                width: `${(teamImageIndexState + 1) / teams.length * 100}%`, border: '1px solid #7BA6B0ff'
+                            }}></div>
+                            <div style={{
+                                position: 'absolute',
+                                top: '25%', left: '-5%', transform: 'translateY(-50%)',
+                                width: '110%', display: 'flex', flexDirection: 'row',
+                                justifyContent: 'space-between'
+                            }}>
+                                <span>01</span>
+                                <span>{teams.length}</span>
                             </div>
-                            <button
-                                style={{
-                                    width: '60px',
-                                    height: '60px',
-                                    borderRadius: '6px',
-                                    fontSize: '2rem',
-                                    backgroundColor: '#5DD1E1ff',
-                                    border: 'none',
-                                    outline: 'none',
-                                    color: '#fff'
-                                }}
-                                disabled={disabledButtonState}
-                                onClick={next}
-                            >
-                                {'>'}
-                            </button>
-                        </div>
-                    </div>
-                    <div className="col-8" style={{ paddingLeft: '50px' }}>
-                        <div>
-                            <h2 style={{fontSize: '3rem'}}>Our Team</h2>
-                            <p style={{fontSize: '1.3rem'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, at? Odit recusandae, hic ratione optio atque magni laudantium dolor sint repellendus deleniti, aperiam similique ad, incidunt molestiae? Esse, quaerat officia.</p>
-                        </div>
-                        <hr style={{ marginLeft: 0, width: '120px', height: '3px', backgroundColor: '#5DD1E1ff' }} />
-                        <div>
-                            <h2>{teams[teamImageIndexState].name}</h2>
-                            <p style={{fontSize: '1.3rem'}}>{teams[teamImageIndexState].roll} | <span>
-                                <img width="16px" height="16px" style={{ marginRight: '5px' }} src={LinkedinSVG} alt="Linkeding" />
-                                <a href={teams[teamImageIndexState].linkedin}>{teams[teamImageIndexState].linkedin}</a>
-                            </span>
-                            </p>
-                            <p style={{fontSize: '1.3rem'}}>
-                                {teams[teamImageIndexState].description}
-                            </p>
 
                         </div>
-                        <div style={{ marginTop: '40px' }}>
-                            <ReactSlickStyle>
-                                <Slider ref={slider} {...settings}
-                                >
-                                    {
-                                        teams.map((img, index) => (
-                                            <img onClick={() => setTeamImageIndexState(index)} src={img.medium} alt="" />
-                                        ))
-                                    }
-                                </Slider>
-                            </ReactSlickStyle>
-                        </div>
-                        
+                        <button
+                            style={{
+                                width: '60px',
+                                height: '60px',
+                                borderRadius: '6px',
+                                fontSize: '2rem',
+                                backgroundColor: '#5DD1E1ff',
+                                border: 'none',
+                                outline: 'none',
+                                color: '#fff'
+                            }}
+                            disabled={disabledButtonState}
+                            onClick={next}
+                        >
+                            {'>'}
+                        </button>
                     </div>
                 </div>
-            </div>
+                <div className="col-8" style={{ paddingLeft: '50px' }}>
+                    <div>
+                        <h2 style={{ fontSize: '3rem' }}>Our Team</h2>
+                        <p style={{ fontSize: '1.3rem' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, at? Odit recusandae, hic ratione optio atque magni laudantium dolor sint repellendus deleniti, aperiam similique ad, incidunt molestiae? Esse, quaerat officia.</p>
+                    </div>
+                    <hr style={{ marginLeft: 0, width: '120px', height: '3px', backgroundColor: '#5DD1E1ff' }} />
+                    <div>
+                        <h2>{teams[teamImageIndexState].name}</h2>
+                        <p style={{ fontSize: '1.3rem' }}>{teams[teamImageIndexState].roll} | <span>
+                            <a href={teams[teamImageIndexState].linkedin}>
+                                <img width="16px" height="16px" style={{ marginRight: '5px' }} src={LinkedinSVG} alt="Linkeding" />
+                            </a>
+                        </span>
+                        </p>
+                        <p style={{ fontSize: '1.3rem' }}>
+                            {teams[teamImageIndexState].description}
+                        </p>
 
-        );
+                    </div>
+                    <div style={{ marginTop: '40px' }}>
+                        <ReactSlickStyle>
+                            <Slider ref={slider} {...settings}
+                            >
+                                {
+                                    teams.map((img, index) => (
+                                        <img onClick={() => setTeamImageIndexState(index)} src={img.medium} alt="" />
+                                    ))
+                                }
+                            </Slider>
+                        </ReactSlickStyle>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    );
 }
